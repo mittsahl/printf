9620f14ba23dcd26ff77f12b9c97da6f800f590b
mittsahl <msalem311@aol.com>
2020-10-23 19:32:36 +0000

    Added struct with chracter and function that returns a string

---
+++ holberton.h
@@ -0,0 +1,23 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * funcStruct - struct with pointer to a char 
+ * @s: pointer to character
+ * @matchedFunction: function that corresponds to character
+ */
+
+typdef struct funcStruct
+{
+	char *s;
+	char *matchedFunction(va_list args);
+} funcStruct;
+
+int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+void _puts(char *str);
+char *_strcpy(char *dest, char *src);
+char *str_concat(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+
+#endif

95b773f5a0d5bae422737e47f2d718408c255b45
mittsahl <msalem311@aol.com>
2020-10-24 18:09:44 +0000

    Added macro max buff size

--- holberton.h
+++ holberton.h
@@ -1,8 +1,11 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
+
+#define MAX_BUF_SIZE 1024
 
 /**
  * funcStruct - struct with pointer to a char 
@@ -10,7 +13,7 @@
  * @matchedFunction: function that corresponds to character
  */
 
-typdef struct funcStruct
+typedef struct funcStruct
 {
 	char *letter;
 	char *(*func)(va_list);
@@ -22,6 +25,7 @@ void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 char *str_concat(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
-char *(*get_op_func(char *s))(va_list);
-
+char *(*get_op_func(const char *s))(va_list);
+int _printf(const char *format, ...);
+char *op_string(va_list args);
 #endif

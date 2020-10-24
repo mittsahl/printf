88749b06d2d7830323a940ebe9b60493dafd23ce
mittsahl <msalem311@aol.com>
2020-10-24 20:15:31 +0000

    Adding to a buffer

--- printf.c
+++ printf.c
@@ -2,41 +2,25 @@
 
 int _printf(const char *format, ...)
 {
-	unsigned int willy = 0;
-        char *string;
-        va_list list;
-
-        va_start(list, format);
-        //while (*(format + willy) != '\0' && (format))
-        //{
-                        funcStruct array[] = {
-				{"s", op_string},
-				{"i", op_int},
-				{"d", op_int},
-				{"u", op_unsignedInt},
-				{"o", op_octal},
-				{"x", op_unsignedHex},
-				{"c", op_char},
-				{"p", op_pointer},
-				{"%", op_percent},
-				{NULL, NULL}};
-        //}
-        printf("\n");
-        va_end(list);
-
-	//Place this in the op_function file
-	char* itoa(int val, int base){
-
-	    static char buf[32] = {0};
-
-	    int i = 30;
-    
-	    for(; val && i ; --i, val /= base){
-		printf("val %d i %d base %d\n", val, i, base);
-		buf[i] = "0123456789ABCDEF"[val % base];
-	    }
-
-	    return &buf[i+1];
-
-	}
+        va_list args;
+	int i = 0;
+	char *str;
+	char *(*function)(va_list);
+	char *buf = malloc(MAX_BUF_SIZE);
+	va_start(args, format);
+	while (*(format + i))
+	{
+		if (*format + i) != '%')
+		
+		if (*(format + i) == '%')
+		{	
+			function = get_op_func((format + i + 1));
+			str = function(args);
+			buf = str_concat(buf, str);		
+			_puts(buf);
+		}
+		i++;
+	}		
+	va_end(args);
+	return (0);
 }

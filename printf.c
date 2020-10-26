965db55ed5d68a3066da2626f55dc0f8b765fc35
mittsahl <msalem311@aol.com>
2020-10-26 16:44:05 +0000

    Works for string and reversed stirngs

--- printf.c
+++ printf.c
@@ -4,41 +4,38 @@
 int _printf(const char *format, ...)
 {
         va_list args;
-	int i = 0;
-	int j = 0;
-	int length = 0;
-	char *str;
-	char *(*function)(va_list);
-	char *buf = malloc(MAX_BUF_SIZE);
-	va_start(args, format);
-	
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		//Edge cases
-		if (format[i] == '\0')
-			return (-1);
-		//Increase both if it is a char
-		if (format[i] != '%')
-			buf[j] = format[i];
-		//get_op_func if it is a %
-		if (format[i] == '%')
-		{	
-			printf("buf before strcat: %s\n", buf);
-			function = get_op_func(*(format + i + 1));
-			str = function(args);
-			_strcat(buf, str);
-			i++;
-			
-			printf("buf after strcat: %s\n", buf);
-			while (buf[j] != '\0')
-				j++;
-			continue;
-		}
-		j++;
-	}			
-	length = _strlen(buf);
-	write(1, buf, length);
-	free(buf);
-	va_end(args);
-	return (0);
+        int i = 0;
+        int j = 0;
+        int length = 0;
+        char *str;
+        char *(*function)(va_list);
+        char *buf = malloc(MAX_BUF_SIZE);
+        va_start(args, format);
+
+        for (i = 0; format[i] != '\0'; i++)
+        {
+                if (format[i] != '%')
+                {
+                        buf[j] = format[i];
+                }
+                if (format[i] == '%')
+                {
+                        function = get_op_func(*(format + i + 1));
+                        str = function(args);
+                        _strcpy(buf, str);
+                        i++;
+                        while (buf[j])
+                        {
+                                j++;
+                        }
+                        continue;
+                }
+                j++;
+        }
+        length = _strlen(buf);
+        write(1, buf, length);
+        free(buf);
+        va_end(args);
+        return (0);
 }
+
